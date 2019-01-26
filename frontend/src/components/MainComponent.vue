41136079168d13fe474d38c14234627ad8b0c153
sevser <semyon-evstigneev@yandex.ru>
2019-01-26 17:57:09 +0300

    Update

---
+++ frontend/src/components/MainComponent.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        MainComponent
+    </div>
+</template>
+
+<script>
+export default {
+    name: "MainComponent"
+}
+</script>
+
+<style scoped>
+div {
+    height: 100%;
+    width: 100%;
+}
+</style>
