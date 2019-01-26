ea640dbb9f691461bf3fff308b2801f882be92fa
Alexander Turkevich <aturkevich@iitdgroup.ru>
2019-01-26 23:36:06 +0300

    Changed IMapper

--- backend/src/main/java/de/jonashackt/springbootvuejs/service/IMapper.java
+++ backend/src/main/java/de/jonashackt/springbootvuejs/service/IMapper.java
@@ -3,11 +3,11 @@ package de.jonashackt.springbootvuejs.service;
 import java.util.List;
 
 /**
- * Интерфейс для маппинга объекта в объект
+ * Интерфейс для маппинга GeoJson в список желаемых объемов
  * @author aleksandr
  * @date 2019-01-26 22:30
  */
-public interface IMapper<T, V> {
+public interface IMapper<T> {
 
-    List<T> map(V object);
+    List<T> map(String geoJson);
 }
