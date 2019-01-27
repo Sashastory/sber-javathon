52260c2d9a9ba74034237b4e3472885e51be47b4
sevser <semyon-evstigneev@yandex.ru>
2019-01-27 08:49:11 +0300

    Финальная версия

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -7,7 +7,7 @@ module.exports = {
   devServer: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8088',
+        target: 'http://localhost:8079',
         ws: true,
         changeOrigin: true,
       },
